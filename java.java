1f12adfb69f1162618dedcd74a7fe7a5a89b6b24
zs <130964825+xiaozhanag@users.noreply.github.com>
2023-04-21 09:24:37 +0800

    Update java.java
    这是一个测试

--- java.java
+++ java.java
@@ -1,5 +1,8 @@
 public static void main(String[] args) {
   
-  System.out.println("hello world")
+    System.out.println("hello world")；
+    System.out.println("hello world")；
+    System.out.println("hello world")；
+    System.out.println("hello world")；
     
 }
